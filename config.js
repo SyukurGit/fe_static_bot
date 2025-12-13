fd32af029d1e9ad1e37ca8830a6cece09c288e09
SyukurGit <220705058@student.ar-raniry.ac.id>
2025-12-13 13:26:21 +0700

    Pesan update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // File: config.js
 const CONFIG = {
     // Ganti URL ini kalau nanti deploy ke VPS atau URL Ngrok berubah
-    API_URL: "https://unbusily-unmoralistic-micki.ngrok-free.dev" 
+    API_URL: "https://dompetpintar.syukurapi.online" 
 };
